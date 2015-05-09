3c2a547c4ec6fdb3542156c27986030312e22789
GuinnessDrewer <lucavianello@MacBook-Pro-di-Luca-Vianello.local>
2015-05-09 13:37:05 +0200

    update MultiEmailSender
    piccolo update

--- Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/MultiEmailSender.java
+++ Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/MultiEmailSender.java
@@ -13,17 +13,31 @@ public class MultiEmailSender {
 
     public void sendEmail(Fall caduta){
 
-        // recupero i dati da inserire nella mail da Fall
+        // recupera i dati da inserire nella mail da Fall
         double[] fallPosition = caduta.getPosition();
         double lat = fallPosition[caduta.FALL_LATITUDE];
         double lon = fallPosition[caduta.FALL_LONGITUDE];
         int num = caduta.getId();
+        String nome = "nomeUtente"; // è il nome di chi cade, magari inserire una opzione nelle settings per impostarlo
 
-        // trasformo i valori in string
+        // trasforma i valori in string
+        String latitudine = ""+lat;
+        String longitudine = ""+lon;
+        String numero = ""+num;
 
-        String latitudine = "pippo";
-        String longitudine = "pluto";
-        String numero = "paperino";
+        // apre un template della mail e lo mette in una stringa per ediatrlo (da finire)
+        String Mail = "NUMERO DATA LATITUDINE LONGITUDINE";
+
+        // inserisco i valori nei campi del testo (cioè parole in maiuscolo del template)
+        Mail.replace("NOME","nome");
+        Mail.replace("NUMERO","numero");
+        Mail.replace("DATA","data");
+        Mail.replace("LATITUDINE","latitudine");
+        Mail.replace("LONGITUDINE","longitudine");
+
+        //compone la mail con destinatari, oggetto e testo
+
+        //invia le mail
 
         // imposta come segnalata la caduta
         caduta.setReported();
