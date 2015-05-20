831fdde4a09d2c465e877c99bfb5f6c40ca39bd0
Sasha Grey <marco.biasin.mb@gmail.com>
2015-05-20 23:40:34 +0200

    Mostra signature
    Per mostrare le signature nella UI 1

--- Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/UI1.java
+++ Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/UI1.java
@@ -164,6 +164,23 @@ public class UI1 extends Activity {
                 from,
                 to);
 
+        adapter.setViewBinder(new SimpleAdapter.ViewBinder(){
+
+            @Override
+            public boolean setViewValue(View view, Object data,
+                                        String textRepresentation) {
+                if( (view instanceof ImageView) & (data instanceof Bitmap) ) {
+                    ImageView iv = (ImageView) view;
+                    Bitmap bm = (Bitmap) data;
+                    iv.setImageBitmap(bm);
+                    return true;
+                }
+                return false;
+
+            }
+
+        });
+
         //utilizzo dell'adapter
         ((ListView)findViewById(R.id.listsessionui1)).setAdapter(adapter);
     }
