cb1ca8cba40c2bdba884c0c17621df6e3a285b5a
moroale93 <moro.ale.93@gmail.com>
2015-05-21 23:22:39 +0200

    dvev

--- Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/NotificationFall.java
+++ Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/NotificationFall.java
@@ -8,7 +8,7 @@ import org.tbw.FemurShield.Observer.Observer;
  * Created by Moro on 19/05/15.
  */
 public interface NotificationFall {
-    public void addObserver(Observer o );
-    public void NotifyAccData(float x, float y, float z);
-    public void NotifyFall(Fall f);
+    void addObserver(Observer o );
+    void NotifyAccData(float x, float y, float z);
+    void NotifyFall(Fall f);
 }
