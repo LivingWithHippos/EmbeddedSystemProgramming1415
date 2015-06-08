bceef91a9a4ee8fab1c3db44bfc16df1764512c2
Marco Biasin <salabracco91@gmail.com>
2015-06-08 12:20:24 +0200

    Accendi schermo allarme
    Lo schermo viene ora correttamente acceso all'apparizione del reminder

--- Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/SessionReminderActivity.java
+++ Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/SessionReminderActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.util.Log;
 import android.view.WindowManager;
 
 import org.tbw.FemurShield.R;
@@ -18,7 +19,7 @@ public class SessionReminderActivity extends Activity implements SessionReminder
     public final String TAG = this.getClass().getSimpleName();
     private PowerManager.WakeLock mWakeLock;
     //secondi prima di spegnere lo schermo
-    private static final int WAKELOCK_TIMEOUT = 20 * 1000;
+    private static final int WAKELOCK_TIMEOUT = 30 * 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,23 +37,22 @@ public class SessionReminderActivity extends Activity implements SessionReminder
             srFragment.show(getFragmentManager(), "SessionReminderDialog");
         }
 
-        //Rimuove il wakelock
-        Runnable releaseWakelock = new Runnable() {
+        // Set the window to keep screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 
-            @Override
-            public void run() {
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        // Acquire wakelock
+        PowerManager pm = (PowerManager) getApplicationContext().getSystemService(Context.POWER_SERVICE);
+        if (mWakeLock == null) {
+            mWakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), TAG);
+        }
 
-                if (mWakeLock != null && mWakeLock.isHeld()) {
-                    mWakeLock.release();
-                }
-            }
-        };
-        //fa spegnere lo schermo dopo 20 secondi di inattivita'
-        new Handler().postDelayed(releaseWakelock, WAKELOCK_TIMEOUT);
+        if (!mWakeLock.isHeld()) {
+            mWakeLock.acquire();
+        }
+
+
     }
 
     @Override
@@ -60,31 +60,15 @@ public class SessionReminderActivity extends Activity implements SessionReminder
         finish();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
 
-        // Set the window to keep screen on
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-
-        // Acquire wakelock
-        PowerManager pm = (PowerManager) getApplicationContext().getSystemService(Context.POWER_SERVICE);
-        if (mWakeLock == null) {
-            mWakeLock = pm.newWakeLock((PowerManager.FULL_WAKE_LOCK | PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), TAG);
-        }
-
-        if (!mWakeLock.isHeld()) {
-            mWakeLock.acquire();
-        }
-
-    }
 
     @Override
     protected void onPause() {
         super.onPause();
+
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 
         if (mWakeLock != null && mWakeLock.isHeld()) {
             mWakeLock.release();
