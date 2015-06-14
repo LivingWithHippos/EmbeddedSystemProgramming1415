d72435d3c04619ac4d9ca6a622da90c9b3dabfd2
Tsirhcitna <salabracco91@gmail.com>
2015-06-14 15:34:54 +0200

    Primo upload UI4

---
+++ Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/FallDetailsFragment.java
@@ -0,0 +1,143 @@
+package org.tbw.FemurShield.Controller;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.tbw.FemurShield.Model.Fall;
+import org.tbw.FemurShield.Model.Session;
+import org.tbw.FemurShield.Model.SessionManager;
+import org.tbw.FemurShield.R;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * to handle interaction events.
+ * Use the {@link FallDetailsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FallDetailsFragment extends Fragment {
+
+    // TODO: Rename and change types of parameters
+    private String sessionID;
+    private String fallID;
+    private String latitude,longitude;
+    private int orientation;
+
+    private LinearLayout ll;
+
+    private Session shownSession;
+    private Fall shownFall;
+
+    private Bitmap sessionSignature;
+    private ImageView ivSessionSignature;
+    private ImageView ivFallSignature;
+    private TextView tvFallDateTime;
+    private TextView tvFallLatitude;
+    private TextView tvFallLongitude;
+
+
+    public static FallDetailsFragment newInstance(String sessionID,String fallID,int orientation) {
+        FallDetailsFragment fragment = new FallDetailsFragment();
+        Bundle args = new Bundle();
+        args.putString(UI4.ID_SESSION, sessionID);
+        args.putString(UI4.ID_FALL, fallID);
+        args.putInt(UI4.ORIENTATION, orientation);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public void setOrientation(int orientation)
+    {
+        this.orientation=orientation;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_fall_details, container, false);
+        //imposto l'orientamento
+        ll=(LinearLayout)rootView.findViewById(R.id.llChangeOnOrientation);
+        ll.setOrientation(orientation);
+        //imposto la signature della sessione
+        ivSessionSignature=(ImageView)rootView.findViewById(R.id.ivSessionSignatureInFallDetails);
+        ivSessionSignature.setImageBitmap(sessionSignature);
+        //TODO: calcolare e impostare l'immagine del fall
+        ivFallSignature=(ImageView)rootView.findViewById(R.id.ivFallSignature);
+        //setto il testo
+        tvFallDateTime=(TextView)rootView.findViewById(R.id.tvFallDetailsDateTime);
+        tvFallDateTime.setText(getString(R.string.fall_date_time)+" "+fallID);
+        tvFallLatitude=(TextView)rootView.findViewById(R.id.tvFallLatitude);
+        tvFallLatitude.setText(getString(R.string.fall_latitude)+" "+latitude);
+        tvFallLongitude=(TextView)rootView.findViewById(R.id.tvFallLongitude);
+        tvFallLongitude.setText(getString(R.string.fall_longitude)+" "+longitude);
+
+        return rootView;
+    }
+
+    public FallDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        //inizializzo le variabili
+        if (getArguments() != null) {
+            sessionID = getArguments().getString(UI4.ID_SESSION);
+            fallID = getArguments().getString(UI4.ID_FALL);
+            orientation=getArguments().getInt(UI4.ORIENTATION);
+        }
+
+        SessionManager sm=SessionManager.getInstance();
+        shownSession=sm.getAllSessionsById().get(sessionID);
+        if(shownSession!=null) {
+            ArrayList<Fall> fallList=shownSession.getFalls();
+            for(Fall f:fallList)
+                if(f.getData().equalsIgnoreCase(fallID))
+                {
+                    shownFall=f;
+                    break;
+                }
+
+            if(shownFall!=null)
+            {
+                sessionSignature = shownSession.getSignature().toBitmap();
+                latitude=shownFall.getPosition()[0]+"";
+                longitude=shownFall.getPosition()[1]+"";
+            }else
+            {
+                Toast.makeText(activity,getString(R.string.no_session_found),Toast.LENGTH_LONG);
+            }
+
+
+        }else
+        {
+            Toast.makeText(activity,getString(R.string.no_session_found),Toast.LENGTH_LONG);
+        }
+    }
+
+
+
+
+}
--- Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/UI4.java
+++ Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/UI4.java
@@ -1,19 +1,44 @@
 package org.tbw.FemurShield.Controller;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import org.tbw.FemurShield.Model.SessionManager;
 import org.tbw.FemurShield.R;
 
 
 public class UI4 extends Activity {
 
+    public static final String ID_FALL = "ID_CADUTA";
+    public static final String ID_SESSION = "ID_SESSIONE";
+    public static final String ORIENTATION = "ORIENTATION";
+    private String fallID,sessionID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ui4);
+
+        sessionID=getIntent().getExtras().getString(ID_SESSION);
+        fallID=getIntent().getExtras().getString(ID_FALL);
+        int orientation=this.getResources().getConfiguration().orientation;
+
+        FallDetailsFragment fdf=FallDetailsFragment.newInstance(sessionID,fallID,orientation);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.ui4rootLayout, fdf, "mFallDetailsFragment");
+        fragmentTransaction.commit();
     }
 
 
@@ -26,16 +51,25 @@ public class UI4 extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, UI5.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_all_sessions:
+                Intent ui1 = new Intent(this, UI1.class);
+                startActivity(ui1);
+                return true;
+            case R.id.action_active_session:
+                if (SessionManager.getInstance().getActiveSession() != null) {
+                    Intent ui3 = new Intent(this, UI3.class);
+                    startActivity(ui3);
+                } else
+                    Toast.makeText(this, getString(R.string.no_active_session), Toast.LENGTH_LONG).show();
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
 }
