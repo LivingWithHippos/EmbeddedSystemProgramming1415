e665f2dde882b9a2aca01aa1258e3fb7b02450ad
Tsirhcitna <salabracco91@gmail.com>
2015-06-15 10:41:44 +0200

    Commentata UI5

--- Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/UI5.java
+++ Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/UI5.java
@@ -19,7 +19,6 @@ import org.tbw.FemurShield.Controller.Settings.AddContactFragment;
 import org.tbw.FemurShield.Controller.Settings.ContactOptionsDialog;
 import org.tbw.FemurShield.Controller.Settings.DurationFragment;
 import org.tbw.FemurShield.Controller.Settings.EmailFragment;
-import org.tbw.FemurShield.Controller.Settings.EmailListItem;
 import org.tbw.FemurShield.Controller.Settings.SampleRatePickerFragment;
 import org.tbw.FemurShield.Controller.Settings.SettingListItem;
 import org.tbw.FemurShield.Controller.Settings.SettingsFragment;
@@ -29,10 +28,13 @@ import org.tbw.FemurShield.Model.SessionManager;
 import org.tbw.FemurShield.R;
 
 /**
-* UI5 e' l'activity che gestisce le impostazioni, contiene il fragment fragment_settings
-* e gestisce il callback dei vari fragment che rappresentano le voci del menu impostazioni
-* */
-public class UI5 extends Activity implements SettingsFragment.OnOptionSelectedListener,TimePickerFragment.OnAlarmChangedListener,DurationFragment.OnDurationChangedListener,EmailFragment.OnAddEmailButtonClickListener,AddContactFragment.OnUserInsertedListener,SampleRatePickerFragment.OnSamplingRateChangedListener,EmailFragment.OnClearEmailClickListener,EmailFragment.OnContactClickListener,ContactOptionsDialog.OnContactOptionsClickListener,AddContactFragment.OnUserToUpdateInsertedListener{
+ * UI5 e' l'activity che gestisce le impostazioni, contiene il fragment fragment_settings
+ * e gestisce il callback dei vari fragment che rappresentano le voci del menu impostazioni {@link SettingsFragment}:
+ * {@link SampleRatePickerFragment}, {@link DurationFragment}, {@link EmailFragment}, {@link TimePickerFragment}
+ *
+ * @author Marco Biasin
+ */
+public class UI5 extends Activity implements SettingsFragment.OnOptionSelectedListener, TimePickerFragment.OnAlarmChangedListener, DurationFragment.OnDurationChangedListener, EmailFragment.OnAddEmailButtonClickListener, AddContactFragment.OnUserInsertedListener, SampleRatePickerFragment.OnSamplingRateChangedListener, EmailFragment.OnClearEmailClickListener, EmailFragment.OnContactClickListener, ContactOptionsDialog.OnContactOptionsClickListener, AddContactFragment.OnUserToUpdateInsertedListener {
 
     private PreferencesEditor prefs;
 
@@ -44,7 +46,7 @@ public class UI5 extends Activity implements SettingsFragment.OnOptionSelectedLi
 
         if (findViewById(R.id.fragment_container_settings) != null) {
 
-            prefs=new PreferencesEditor(this);
+            prefs = new PreferencesEditor(this);
 
             /*Se stiamo tornando indietro non abbiamo bisogno di ricaricarlo
             * rischiamo di ritrovarci con un secondo fragment sovrapposto*/
@@ -62,7 +64,7 @@ public class UI5 extends Activity implements SettingsFragment.OnOptionSelectedLi
             FragmentManager fragmentManager = getFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-            fragmentTransaction.add(R.id.fragment_container_settings, settFragment ,"mSettingsFragment");
+            fragmentTransaction.add(R.id.fragment_container_settings, settFragment, "mSettingsFragment");
             fragmentTransaction.commit();
         }
     }
@@ -75,26 +77,29 @@ public class UI5 extends Activity implements SettingsFragment.OnOptionSelectedLi
         return true;
     }
 
+    /**
+     * Gestisce il menu in alto a destra.
+     *
+     * @param item la voce del menu selezionata
+     * @return true se l'evento e' stato consumato, altrimenti viene passato al menu "superiore"
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
 
-        switch(item.getItemId())
-        {
+        switch (item.getItemId()) {
             case R.id.action_all_sessions:
-                Intent ui1=new Intent(this,UI1.class);
+                Intent ui1 = new Intent(this, UI1.class);
                 startActivity(ui1);
                 return true;
             case R.id.action_active_session:
-                if(SessionManager.getInstance().getActiveSession()!=null)
-                {
-                    Intent ui3=new Intent(this,UI3.class);
+                if (SessionManager.getInstance().getActiveSession() != null) {
+                    Intent ui3 = new Intent(this, UI3.class);
                     startActivity(ui3);
-                }
-                else
-                    Toast.makeText(this,getString(R.string.no_active_session),Toast.LENGTH_LONG).show();
+                } else
+                    Toast.makeText(this, getString(R.string.no_active_session), Toast.LENGTH_LONG).show();
                 return true;
         }
 
@@ -102,30 +107,35 @@ public class UI5 extends Activity implements SettingsFragment.OnOptionSelectedLi
     }
 
     /**
+     * Salva la nuova durata massima della sessione e aggiorna la vista nel menu {@link SettingsFragment}
+     *
      * @param newDuration indica la nuova durata massima della sessione scelta.
-     * Questa � l'implementazione dell'interfaccia di callback dichirata nel DurationFragment
+     *                    Questa e' l'implementazione dell'interfaccia di callback dichirata nel DurationFragment
      */
     @Override
     public void onDurationChanged(int newDuration) {
 
         prefs.setSessionDuration(newDuration);
-        SettingsFragment settingsFragment = (SettingsFragment)getFragmentManager().findFragmentById(R.id.fragment_container_settings);
-        if(settingsFragment !=null)
+        SettingsFragment settingsFragment = (SettingsFragment) getFragmentManager().findFragmentById(R.id.fragment_container_settings);
+        if (settingsFragment != null)
             settingsFragment.updateSessionDuration(newDuration);
 
     }
 
     /**
-    *  @param hourOfDay indica l'ora selezionata nel Time Picker per l'allarme
-    *  @param minute indica il minuto selezionata nel Time Picker per l'allarme
-    *  Implementazione dell'interfaccia di callback dichiarata in TimePickerFragment
-    */
+     * Metodo che gestisce il nuovo orario dell'allarme selezionato in {@link TimePickerFragment}.
+     * Fa partire il servizio di sveglia {@link ReminderService}
+     *
+     * @param hourOfDay indica l'ora selezionata nel Time Picker per l'allarme
+     * @param minute    indica il minuto selezionata nel Time Picker per l'allarme
+     *                  Implementazione dell'interfaccia di callback dichiarata in TimePickerFragment
+     */
     @Override
     public void OnAlarmChanged(int hourOfDay, int minute) {
 
         //aggiorna l'orario mostrato nella descrizione
-        SettingsFragment alarmFrag = (SettingsFragment)getFragmentManager().findFragmentById(R.id.fragment_container_settings);
-        if(alarmFrag!=null)
+        SettingsFragment alarmFrag = (SettingsFragment) getFragmentManager().findFragmentById(R.id.fragment_container_settings);
+        if (alarmFrag != null)
             alarmFrag.updateAlarmTime(hourOfDay, minute);
 
         Intent alarmService = new Intent(this, ReminderService.class);
@@ -143,132 +153,170 @@ public class UI5 extends Activity implements SettingsFragment.OnOptionSelectedLi
                 PackageManager.DONT_KILL_APP);
     }
 
+    /**
+     * Metodo lanciato alla pressione del pulsante "+" {@link } nella schermata dei contatti {@link EmailFragment}.
+     * Fa comparire un dialog {@link AddContactFragment} che permette di inserirne uno nuovo.
+     */
     @Override
     public void onAddEmailButtonClick() {
-        AddContactFragment emailFragment=new AddContactFragment();
-        Bundle bundle=new Bundle();
-        bundle.putBoolean(AddContactFragment.CONTACT_MODE,AddContactFragment.MODE_NEW_USER);
+        AddContactFragment emailFragment = new AddContactFragment();
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(AddContactFragment.CONTACT_MODE, AddContactFragment.MODE_NEW_USER);
         emailFragment.setArguments(bundle);
         emailFragment.show(getFragmentManager(), "Add Contact Dialog");
     }
 
+    /**
+     * Metodo lanciato all'inserimento di un nuovo contatto per l'email, {@link UI5#onAddEmailButtonClick()}
+     * salva i dati se corretti.
+     *
+     * @param nome      EditText in cui e stato inserito il nome
+     * @param indirizzo EditText in cui e stato inserita l email
+     * @return true se viene salvato (file ok e email non gia' presente) o false altrimenti
+     */
     @Override
     public boolean onUserInserted(EditText nome, EditText indirizzo) {
-        boolean result=true;
-        if(nome!=null&indirizzo!=null)
-        {
-            String n=nome.getText().toString().trim();
-            String i=indirizzo.getText().toString().trim();
-            Log.d("UI5","nome :"+n);
-            Log.d("UI5","indirizzo :"+i);
+        boolean result = true;
+        if (nome != null & indirizzo != null) {
+            String n = nome.getText().toString().trim();
+            String i = indirizzo.getText().toString().trim();
+            Log.d("UI5", "nome :" + n);
+            Log.d("UI5", "indirizzo :" + i);
 
-            String regex="[\\w_.-]+@[\\w.-]{4,30}";
-            if(i.matches(regex))
-            {
-                result=prefs.addEmail(i,n);
-                Log.d("UI5","ho modificato : "+result);
+            String regex = "[\\w_.-]+@[\\w.-]{4,30}";
+            if (i.matches(regex)) {
+                result = prefs.addEmail(i, n);
+                Log.d("UI5", "ho modificato : " + result);
 
-                if(result) {
+                if (result) {
                     //aggiorna la lista email
                     EmailFragment ef = (EmailFragment) getFragmentManager().findFragmentById(R.id.fragment_container_settings);
                     ef.updateList();
-                }
-                else
-                    Toast.makeText(getApplicationContext(), getString(R.string.wrong_email_message),Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(getApplicationContext(), getString(R.string.wrong_email_message), Toast.LENGTH_SHORT).show();
 
-            }
-            else{
-                Log.d("UI5","Sintassi email errata");
-                result=false;
-                Toast.makeText(getApplicationContext(), getString(R.string.wrong_email_message),Toast.LENGTH_SHORT).show();
+            } else {
+                Log.d("UI5", "Sintassi email errata");
+                result = false;
+                Toast.makeText(getApplicationContext(), getString(R.string.wrong_email_message), Toast.LENGTH_SHORT).show();
             }
         }
         return result;
     }
 
+    /**
+     * Metodo lanciato dopo aver premuto modifica sul menu a comparsa di un contatto  {@link ContactOptionsDialog}
+     * e aver premuto ok dopo averlo modificato nel Dialog {@link AddContactFragment}
+     *
+     * @param nome      l'EditText contenente il nuovo nome
+     * @param indirizzo l'EditText contenente il nuovo indirizzo
+     * @param oldEmail  la vecchia email, usata come chiave per riconoscere che contatto rimpiazzare
+     * @return true se viene rimpiazzato correttamente
+     */
     @Override
     public boolean onUserToUpdateInserted(EditText nome, EditText indirizzo, String oldEmail) {
 
-        boolean result=false;
+        boolean result = false;
         result = prefs.deleteContact(oldEmail);
-        if(result)
-            result=onUserInserted(nome,indirizzo);
+        if (result)
+            result = onUserInserted(nome, indirizzo);
 
         return result;
     }
 
+    /**
+     * Salva la frequenza di campionamento scelta del sensore tramite il Dialog {@link SampleRatePickerFragment} e aggiorna la vista nel menu {@link SettingsFragment}
+     *
+     * @param newRate il nuovo valore da salvare
+     */
     @Override
     public void onSamplingRateChanged(int newRate) {
-        SettingsFragment settingsFragment=(SettingsFragment)getFragmentManager().findFragmentById(R.id.fragment_container_settings);
+        SettingsFragment settingsFragment = (SettingsFragment) getFragmentManager().findFragmentById(R.id.fragment_container_settings);
         settingsFragment.updateSamplingRate(newRate);
     }
 
+    /**
+     * Metodo chiamato alla pressione del tasto "Elimina Contatti"
+     * nel menu {@link UI5#onOptionsItemSelected(MenuItem)}.
+     * Elimina tutti i contatti e aggiorna la lista contatti svuotandola.
+     */
     @Override
     public void onClearEmail() {
         prefs.cleanEmailFile();
-        EmailFragment ef=(EmailFragment)getFragmentManager().findFragmentById(R.id.fragment_container_settings);
+        EmailFragment ef = (EmailFragment) getFragmentManager().findFragmentById(R.id.fragment_container_settings);
         ef.clearList();
-
     }
 
+    /**
+     * Metodo chiamato alla pressione prolungata su un contatto, permette di modificarlo o eliminarlo.
+     * Mostra il Dialog {@link ContactOptionsDialog}
+     *
+     * @param emailAddress l'indirizzo del contatto selezionato
+     * @param name         il nome del contatto selezionato
+     */
     @Override
-    public void onContactLongClick(String emailAddress,String name) {
-        ContactOptionsDialog dialog=new ContactOptionsDialog();
-        Bundle bundle=new Bundle();
+    public void onContactLongClick(String emailAddress, String name) {
+        ContactOptionsDialog dialog = new ContactOptionsDialog();
+        Bundle bundle = new Bundle();
         bundle.putString(ContactOptionsDialog.SELECTED_MAIL, emailAddress);
-        bundle.putString(ContactOptionsDialog.SELECTED_NAME,name);
+        bundle.putString(ContactOptionsDialog.SELECTED_NAME, name);
         dialog.setArguments(bundle);
         dialog.show(getFragmentManager(), "Contact Options Menu");
     }
 
+    /**
+     * Metodo che rileva la pressione su "modifica" o "elimina"
+     * nel menu a comparsa dei contatti {@link ContactOptionsDialog}
+     *
+     * @param emailAddress l'indirizzo del contatto selezionato
+     * @param name         il nome del contatto selezionato
+     * @param type         se si tratta di eliminazione ({@link ContactOptionsDialog#DELETE_CONTACT})o modifica ({@link ContactOptionsDialog#EDIT_CONTACT}) del contatto.
+     */
     @Override
     public void onContactOptionClick(String emailAddress, String name, int type) {
-        switch (type)
-        {
+        switch (type) {
             case ContactOptionsDialog.DELETE_CONTACT:
-                if(prefs.deleteContact(emailAddress))
-                {
-                    EmailFragment ef=(EmailFragment)getFragmentManager().findFragmentById(R.id.fragment_container_settings);
+                if (prefs.deleteContact(emailAddress)) {
+                    EmailFragment ef = (EmailFragment) getFragmentManager().findFragmentById(R.id.fragment_container_settings);
                     ef.updateList();
-                }else
-                {
+                } else {
                     Toast.makeText(this, getString(R.string.contact_not_deleted), Toast.LENGTH_LONG).show();
                 }
                 break;
             case ContactOptionsDialog.EDIT_CONTACT:
-                AddContactFragment emailFragment=new AddContactFragment();
-                Bundle bundle=new Bundle();
-                bundle.putBoolean(AddContactFragment.CONTACT_MODE,AddContactFragment.MODE_EDIT_USER);
+                AddContactFragment emailFragment = new AddContactFragment();
+                Bundle bundle = new Bundle();
+                bundle.putBoolean(AddContactFragment.CONTACT_MODE, AddContactFragment.MODE_EDIT_USER);
                 bundle.putString(AddContactFragment.OLD_EMAIL, emailAddress);
-                bundle.putString(AddContactFragment.OLD_NAME,name);
+                bundle.putString(AddContactFragment.OLD_NAME, name);
                 emailFragment.setArguments(bundle);
-                emailFragment.show(getFragmentManager(),"Add Contact Dialog");
+                emailFragment.show(getFragmentManager(), "Add Contact Dialog");
                 break;
         }
     }
 
-
+    /**
+     * Metodo che gestisce le voci scelte nel menu {@link SettingsFragment}
+     *
+     * @param s l'opzione scelta dalla lista
+     */
     @Override
     public void onOptionSelected(SettingListItem s) {
-        
-        if(s.title.equalsIgnoreCase(getString(R.string.title_alarm)))
-        {
-            TimePickerFragment timepick= new TimePickerFragment();
+
+        if (s.title.equalsIgnoreCase(getString(R.string.title_alarm))) {
+            TimePickerFragment timepick = new TimePickerFragment();
             timepick.show(getFragmentManager(), "TimePicker");
         }
-        if(s.title.equalsIgnoreCase(getString(R.string.title_sample_rate)))
-        {
-            SampleRatePickerFragment slider=new SampleRatePickerFragment();
-            slider.show(getFragmentManager(),"SampleRatePicker");
+        if (s.title.equalsIgnoreCase(getString(R.string.title_sample_rate))) {
+            SampleRatePickerFragment slider = new SampleRatePickerFragment();
+            slider.show(getFragmentManager(), "SampleRatePicker");
         }
-        if(s.title.equalsIgnoreCase(getString(R.string.title_session_duration)))
-        {
-            DurationFragment duration=new DurationFragment();
+        if (s.title.equalsIgnoreCase(getString(R.string.title_session_duration))) {
+            DurationFragment duration = new DurationFragment();
             duration.show(getFragmentManager(), "DurationPicker");
         }
-        if(s.title.equalsIgnoreCase(getString(R.string.title_email_recipient)))
-        {
-            EmailFragment emailFragment=new EmailFragment();
+        if (s.title.equalsIgnoreCase(getString(R.string.title_email_recipient))) {
+            EmailFragment emailFragment = new EmailFragment();
             FragmentTransaction transaction = getFragmentManager().beginTransaction();
 
             // rimpiazza il fragment attuale e lo aggiunge allo stack in modo che premendo indietro ricompaia
@@ -278,8 +326,8 @@ public class UI5 extends Activity implements SettingsFragment.OnOptionSelectedLi
             // Commit the transaction
             transaction.commit();
         }
-        if(s.title.equalsIgnoreCase("Simula Caduta"))
-            Controller.getNotification().NotifyFall(new Fall(null,null,null, getBaseContext()));
+        if (s.title.equalsIgnoreCase("Simula Caduta"))
+            Controller.getNotification().NotifyFall(new Fall(null, null, null, getBaseContext()));
 
     }
 }
