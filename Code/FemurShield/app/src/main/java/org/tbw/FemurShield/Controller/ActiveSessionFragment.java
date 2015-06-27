5cc7fa6a15ae885fb70f276c252f4a05d7b429a2
Tsirhcitna <salabracco91@gmail.com>
2015-06-27 11:54:42 +0200

    Files per fragment UI3
    Non è ancora applicata

---
+++ Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/ActiveSessionFragment.java
@@ -0,0 +1,193 @@
+package org.tbw.FemurShield.Controller;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.tbw.FemurShield.Model.Fall;
+import org.tbw.FemurShield.Model.SessionManager;
+import org.tbw.FemurShield.Observer.Observable;
+import org.tbw.FemurShield.R;
+
+/**
+ * Created by Marco on 26/06/2015.
+ */
+public class ActiveSessionFragment extends Fragment implements org.tbw.FemurShield.Observer.Observer{
+
+    float distCentro=400.0f;
+    float larghezzachart;
+
+    float yIndexAcc= 0.0f;
+    float xOldXAcc=distCentro;
+    float xOldYAcc=distCentro;
+    float xOldZAcc=distCentro;
+
+    Canvas canvasGraficoAcc;
+    Bitmap bitmapGraficoAcc;
+
+    Paint paintX = new Paint();
+    Paint paintY = new Paint();
+    Paint paintZ = new Paint();
+    Paint paintCanc = new Paint();
+    Paint line= new Paint();
+
+    ImageView imageViewGraficoAcc;
+
+    OnFallListener fallCallback;
+
+    public ActiveSessionFragment()
+    {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        paintX.setColor(Color.BLACK);
+        paintX.setStrokeWidth(6.0f);
+
+        paintY.setColor(Color.RED);
+        paintY.setStrokeWidth(6.0f);
+
+        paintZ.setColor(Color.GREEN);
+        paintZ.setStrokeWidth(6.0f);
+
+        paintCanc.setColor(Color.rgb(241, 241, 241));
+        paintCanc.setStyle(Paint.Style.FILL);
+
+        Paint line= new Paint();
+        line.setColor(Color.MAGENTA);
+        line.setStrokeWidth(3.0f);
+
+    }
+
+    public static ActiveSessionFragment newIstance()
+    {
+        ActiveSessionFragment fragment = new ActiveSessionFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            fallCallback = (OnFallListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFallListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        fallCallback = null;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_active_session, container,false);
+
+
+        imageViewGraficoAcc= (ImageView)rootView.findViewById(R.id.graficoacc);
+        bitmapGraficoAcc = Bitmap.createBitmap( getActivity().getWindowManager()
+                .getDefaultDisplay().getWidth(), getActivity().getWindowManager()
+                .getDefaultDisplay().getHeight()/2, Bitmap.Config.ARGB_8888);
+        canvasGraficoAcc = new Canvas(bitmapGraficoAcc);
+
+        canvasGraficoAcc.drawLine(0, distCentro, getActivity().getWindowManager()
+                .getDefaultDisplay().getWidth(), distCentro, line);
+        imageViewGraficoAcc.setImageBitmap(bitmapGraficoAcc);
+        imageViewGraficoAcc.draw(canvasGraficoAcc);
+        larghezzachart=getActivity().getWindowManager().getDefaultDisplay().getWidth();
+        Controller.getNotification().addObserver(this);
+
+        Chronometer chrono=(Chronometer) rootView.findViewById(R.id.chronometerui3);
+        chrono.setBase(Controller.getInstance().getActualChronoBase());
+        if(Controller.getInstance().isRunning()){
+            chrono.start();
+        }
+
+        return rootView;
+    }
+
+
+    public void startChrono()
+    {
+        Chronometer chrono=(Chronometer) getView().findViewById(R.id.chronometerui3);
+        chrono.setBase(Controller.getInstance().getActualChronoBase());
+        chrono.start();
+    }
+
+    public void stopChrono()
+    {
+        Chronometer chrono=(Chronometer) getView().findViewById(R.id.chronometerui3);
+        chrono.stop();
+    }
+
+    @Override
+    public void update(Observable oggettoosservato, Object o) {
+        if(o instanceof float[]){
+            float[] arg=(float[])o;
+            DrawGraphSliceAcc(arg[0]*6, arg[1]*6, arg[2]*6);
+        }
+        if(o instanceof Fall)
+            fallCallback.onFall();
+
+    }
+
+    public void DrawGraphSliceAcc(float newX, float newY, float newZ){
+        EraseNextAcc(yIndexAcc+1.0f);
+        DrawSegmentXAcc(yIndexAcc, xOldXAcc, yIndexAcc + 1.0f, newX + distCentro);
+        xOldXAcc=newX+distCentro;
+        DrawSegmentYAcc(yIndexAcc, xOldYAcc, yIndexAcc + 1.0f, newY + distCentro);
+        xOldYAcc=newY+distCentro;
+        DrawSegmentZAcc(yIndexAcc, xOldZAcc, yIndexAcc + 1.0f, newZ + distCentro);
+        xOldZAcc=newZ+distCentro;
+
+
+        if(yIndexAcc>=larghezzachart){
+            yIndexAcc=0;
+        }
+        else{
+            yIndexAcc+=1;
+        }
+    }
+
+    private void EraseNextAcc(float v) {
+        canvasGraficoAcc.drawRect(v, 0, v + 15.0f, 3000, paintCanc);
+        imageViewGraficoAcc.setImageBitmap(bitmapGraficoAcc);
+    }
+
+    private void DrawSegmentXAcc( float xStart, float yStart, float xStop, float yStop){
+        canvasGraficoAcc.drawLine(xStart, yStart, xStop, yStop, paintX);
+        imageViewGraficoAcc.setImageBitmap(bitmapGraficoAcc);
+    }
+
+    private void DrawSegmentYAcc(float xStart, float yStart, float xStop, float yStop){
+        canvasGraficoAcc.drawLine(xStart, yStart, xStop, yStop, paintY);
+        imageViewGraficoAcc.setImageBitmap(bitmapGraficoAcc);
+    }
+
+    private void DrawSegmentZAcc(float xStart, float yStart, float xStop, float yStop){
+        canvasGraficoAcc.drawLine(xStart, yStart, xStop, yStop, paintZ);
+        imageViewGraficoAcc.setImageBitmap(bitmapGraficoAcc);
+    }
+
+    public interface OnFallListener
+    {
+        public void onFall();
+    }
+}
---
+++ Code/FemurShield/app/src/main/java/org/tbw/FemurShield/Controller/UI3Neo.java
@@ -0,0 +1,106 @@
+package org.tbw.FemurShield.Controller;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import org.tbw.FemurShield.Model.SessionManager;
+import org.tbw.FemurShield.R;
+
+/**
+ * Created by Marco on 27/06/2015.
+ */
+public class UI3Neo extends BaseActivity implements FallFragment.OnFallClickListener,SessionCommandsFragment.OnCommandUpdatedListener,ActiveSessionFragment.OnFallListener{
+
+    private String thisData;
+    private int layoutID;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui3neo);
+
+        LinearLayout fragContainer = (LinearLayout) findViewById(R.id.ui3rootLayout);
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        layoutID=View.generateViewId();
+        ll.setId(layoutID);
+
+        thisData= SessionManager.getInstance().getActiveSession().getDataTime();
+
+        SessionDetailsFragment sdf = SessionDetailsFragment.newIstance(thisData,SessionDetailsFragment.UI_3_MODE);
+        ActiveSessionFragment asf= ActiveSessionFragment.newIstance();
+        SessionCommandsFragment scf=SessionCommandsFragment.newInstance(SessionCommandsFragment.MODE_SMALL);
+        FallFragment ff = FallFragment.newInstance(thisData);
+
+        getFragmentManager().beginTransaction().add(ll.getId(),sdf, "sessionDetails").commit();
+        getFragmentManager().beginTransaction().add(ll.getId(),asf, "activeSessionGraph").commit();
+        getFragmentManager().beginTransaction().add(ll.getId(),scf, "sessionCommand").commit();
+        getFragmentManager().beginTransaction().add(ll.getId(), ff, "fallsList_ui3").commit();
+
+        fragContainer.addView(ll);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_ui2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, UI5.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_all_sessions:
+                Intent ui1 = new Intent(this, UI1.class);
+                startActivity(ui1);
+                return true;
+            case R.id.action_active_session:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onFallClick(String sessionID, String fallID) {
+        Intent fallDetails=new Intent(this,UI4.class);
+        fallDetails.putExtra(UI4.ID_SESSION, sessionID);
+        fallDetails.putExtra(UI4.ID_FALL, fallID);
+        startActivity(fallDetails);
+        }
+
+    public void aggiornaLista(){
+        //do nothing
+    }
+
+    @Override
+    public void aggiornaLista(int buttonPressed) {
+        switch (buttonPressed) {
+            case SessionCommandsFragment.BUTTON_PAUSE:
+                ((ActiveSessionFragment)getFragmentManager().findFragmentByTag("activeSessionGraph")).stopChrono();
+                break;
+            case SessionCommandsFragment.BUTTON_PLAY:
+                ((ActiveSessionFragment)getFragmentManager().findFragmentByTag("activeSessionGraph")).startChrono();
+                break;
+            case SessionCommandsFragment.BUTTON_STOP:
+                ((ActiveSessionFragment)getFragmentManager().findFragmentByTag("activeSessionGraph")).stopChrono();
+                break;
+        }
+    }
+
+    @Override
+    public void onFall() {
+        ((FallFragment) getFragmentManager().findFragmentByTag("fallsList_ui3")).startlist();
+    }
+}
+
